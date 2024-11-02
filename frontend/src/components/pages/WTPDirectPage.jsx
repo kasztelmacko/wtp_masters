e6fe7c7a513e5462a6c05ff063d51d98525e2b7a
kasztelmacko <152562189+kasztelmacko@users.noreply.github.com>
2024-11-02 13:48:33 +0100

    styled item card so it looks more like Wendys

--- frontend/src/components/pages/WTPDirectPage.jsx
+++ frontend/src/components/pages/WTPDirectPage.jsx
@@ -23,27 +23,27 @@ const WTPDirectPage = ({
     const itemOptions = {
         burger: {
             item_id: 'burger',
-            item_name: 'Classic Burger',
-            item_description: 'A delicious classic burger with lettuce, tomato, and cheese.',
-            img_url: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/bundle_classic.png?t=2024-10-17T17%3A35%3A25.055Z',
-            portion_size: 500,
-            calories: 500
+            item_name: 'Dave`s Single',
+            item_description: 'fresh British beef, American cheese, lettuce, tomato, pickle, ketchup, mayo, and onion',
+            img_url: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/burger_classic.png',
+            portion_size: 280,
+            calories: 524
         },
         burger_premium: {
             item_id: 'burger_premium',
-            item_name: 'Premium Burger',
-            item_description: 'A premium burger with extra toppings and sauces.',
-            img_url: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/bundle_classic.png?t=2024-10-17T17%3A35%3A25.055Z',
-            portion_size: 600,
-            calories: 600
+            item_name: 'Baconator',
+            item_description: 'fresh British beef, American cheese, 6 pieces of crispy bacon, ketchup, mayo',
+            img_url: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/burger_premium.png',
+            portion_size: 350,
+            calories: 1010
         },
         bundle: {
             item_id: 'bundle',
-            item_name: 'Family Bundle',
-            item_description: 'A family bundle with burgers, fries, and drinks.',
-            img_url: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/bundle_classic.png?t=2024-10-17T17%3A35%3A25.055Z',
-            portion_size: 1000,
-            calories: 1200
+            item_name: 'Dave`s Single Combo',
+            item_description: 'fresh British beef, American cheese, lettuce, tomato, pickle, ketchup, mayo, and onion, fries, and a drink',
+            img_url: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/bundle_classic.png',
+            portion_size: 470,
+            calories: 930
         }
     };
 
@@ -98,35 +98,48 @@ const WTPDirectPage = ({
         <FormWrapper onSubmit={handleSubmit} isLastPage={true}>
             <div className="flex flex-col items-center justify-center">
                 {/* Item Card */}
-                <div className="mb-4">
-                    <div className="h-full relative group flex flex-col border rounded-lg shadow-md p-4 w-full max-w-xs">
-                        <div className='h-48 content-center'>
-                            <img 
-                                className="rounded-t-lg w-full h-full object-cover" 
-                                src={option.img_url} 
-                                alt="" 
-                                draggable="false"
-                            />
+                <div className="mb-8">
+                    <div className="h-full relative group flex flex-col border rounded-lg shadow-md pt-4 w-full max-w-xs">
+                        <div className='h-48 w-full flex justify-center items-center'>
+                            <figure>
+                                <img src={option.img_url}  
+                                className="max-w-full max-h-full object-contain" 
+                                />
+                            </figure>
                         </div>
-                        <div className="p-5 w-full flex flex-col flex-grow overflow-hidden">
-                            <a className="block w-full" draggable="false">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{option.item_name}</h5>
-                            </a>
-                            <p className="mb-3 text-xs text-gray-700 dark:text-gray-400 overflow-hidden leading-4 flex-grow">
-                                {option.item_description}
-                            </p>
-                            <div className="text-xs font-semibold text-gray-700 dark:text-gray-400 mb-3">
-                                {option.portion_size} g, {option.calories} kcal
+                        <div className="w-full flex flex-col flex-grow overflow-hidden">
+                            <div className="p-5">
+                                <a className="block w-full" draggable="false">
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{option.item_name}</h5>
+                                </a>
+                                <p className="mb-3 text-xs text-gray-700 dark:text-gray-400 overflow-hidden leading-4 flex-grow">
+                                    {option.item_description}
+                                </p>
                             </div>
-                            <div className="mt-auto flex items-center gap-x-1">
-                                <div className="text-5xl font-bold tracking-tight text-gray-900">
-                                    ??? <span className="text-xl">zł</span>
+                            <div className="mt-auto w-full">
+                                <div className="bg-red-500 text-white text-xs font-semibold p-2 rounded-md grid grid-cols-3 gap-4 items-center justify-items-center">
+                                    <div className="text-center">
+                                        <div>{option.portion_size} g</div>
+                                        <div>{option.calories} kcal</div>
+                                    </div>
+                                    <div className="text-center ">
+                                    <div className="w-8 rounded">
+                                        <img
+                                        src="https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos/wendys-logo-6.png?t=2024-11-02T12%3A17%3A11.521Z"
+                                        alt="wendy's logo" />
+                                    </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <div className="text-5xl font-bold tracking-tight text-white">
+                                            ??? <span className="text-xl">zł</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
+    
                 {/* Input Questions */}
                 <div>
                     <p>At what price would you consider this burger so expensive that you <span className="font-bold">would not</span> consider buying it?</p>
